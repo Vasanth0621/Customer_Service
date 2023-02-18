e2889c3d0f294bc87c4febd15c22e322da908e7d
Vasanth0621 <114357165+Vasanth0621@users.noreply.github.com>
2023-02-18 14:33:11 +0530

    Add files via upload

---
+++ customer_service-2/src/main/java/com/projectMC/demo/CustomerService2Application.java
@@ -0,0 +1,13 @@
+package com.projectMC.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerService2Application.class, args);
+	}
+
+}
---
+++ customer_service-2/src/main/java/com/projectMC/demo/Customer_Service_Repository.java
@@ -0,0 +1,8 @@
+package com.projectMC.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Customer_Service_Repository extends JpaRepository<Customer, Integer> 
+{
+
+}
